2a8d6c0caff0cc92be42f14306ae301f405815c3
Your Name <you@example.com>
2023-03-22 21:20:30 +0600

    first commit

---
+++ assignment1.js
@@ -0,0 +1,10 @@
+function multiplicationTable(num){
+    var i = 1;
+    for(i = 1 ; i <11 ; i++){
+        var product = num * i ; 
+        console.log(num, "*", i, "=", product)
+    }
+}
+
+multiplicationTable(3)
+console.log(23)
